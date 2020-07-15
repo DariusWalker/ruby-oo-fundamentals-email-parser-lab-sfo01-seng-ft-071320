39ce9112ebc74e55ac78cc4760fbe36dd81b689f
Darius Walker <dw862716@ohio.edu>
2020-07-15 06:33:59 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,12 +5,18 @@
 class EmailAddressParser 
   attr_accessor :email_addresses 
   
+  
+  
   def initialize emailaddresses
     @email_addresses = email_addresses
   end 
+  
+  
+  
   def parse 
     return @email_addresses.split(/,? /).uniq 
   end
+
 end 
 
     
